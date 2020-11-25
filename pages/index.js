52380e4643ccc09507bddd5a8a2d8cbaaba5ae4a
Humaid Khan <humaidk2@gmail.com>
2020-11-25 16:30:32 +0400

    build page with canvas, audio, play btn, and rain elements

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,170 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="UTF-8" />
+        <title>Undertale Tribute</title>
+        <script src="/all.min.js"></script>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <i className="rain"></i>
+      <canvas id="mycanvas"></canvas>
+      <button id="play-btn">
+        <i className="fas fa-play"></i>
+      </button>
+      <audio id="audioElement"></audio>
+      <audio id="rain" src="rain2.mp3"></audio>
+      <script src="script.js"></script>
     </div>
-  )
+  );
 }
